fe9124460755f34c441dbe42f29032a8fafe4eb5
Stephen Stephenson <stephenson.inbox@gmail.com>
2023-04-03 14:54:47 +0200

    Style: cs fixer

---
+++ src/class/Action/Extract.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Backup\Action;
+
+class Decrypt {
+    private $folder;
+
+    private $encryption_list;
+
+    public function __construct($folder, $encryption_list) {
+        $this->folder = $folder;
+        $this->$encryption_list = $encryption_list;
+
+        // TODO: To check if folder exists
+    }
+
+    public function do() {
+    }
+}
